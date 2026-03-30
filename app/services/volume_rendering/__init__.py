994a5bedfdcdbe56b1d04f71eba5a7f0d5a0855a
jun <z31014047@163.com>
2026-03-30 17:04:14 +0800

    add 3D

---
+++ app/services/volume_rendering/__init__.py
@@ -0,0 +1,11 @@
+from app.services.volume_rendering.vtk_volume_renderer import (
+    VolumeRenderRequest,
+    VtkVolumeRenderer,
+    vtk_volume_renderer,
+)
+
+__all__ = [
+    "VolumeRenderRequest",
+    "VtkVolumeRenderer",
+    "vtk_volume_renderer",
+]
